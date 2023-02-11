f8638ae3c6431248965dfcf7fb38b44e290894df
GBachheimer <107429234+GBachheimer@users.noreply.github.com>
2023-02-11 21:10:39 +0200

    products main page/table and empty card template

--- src/navigation/screens/Products/Product.tsx
+++ src/navigation/screens/Products/Product.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import ProductsHeader from './components/ProductsHeader';
+import ProductsTable from './components/ProductsTable';
 
 const Product = () => {
   return (
-    <View>
-      <Text>Product</Text>
+    <View style={styles.container}>
+      <ProductsHeader />
+      <ProductsTable />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default Product;
---
+++ src/navigation/screens/Products/components/ProductsHeader.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Switch } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+const ProductsHeader = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.text}>Products (24 / 100)</Text>
+        <Text style={styles.text}>Featured Products (7 / 10)</Text>
+        <View style={styles.toggle}>
+          <Switch
+            trackColor={{ false: '#767577', true: '#AED6F1' }}
+            thumbColor={isEnabled ? '#21B8F9' : '#f4f3f4'}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleSwitch}
+            value={isEnabled}
+          />
+        </View>
+      </View>
+      <View>
+        <TouchableOpacity style={styles.button}>
+          <MaterialIcons name="add" size={30} color="#fff" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: 150,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  button: {
+    textAlign: 'center',
+    padding: 15,
+    backgroundColor: '#21B8F9',
+    borderRadius: 5,
+    marginRight: 15,
+  },
+  text: {
+    padding: 5,
+    paddingLeft: 15,
+    color: '#546679',
+  },
+  toggle: {
+    width: '30%',
+  },
+});
+
+export default ProductsHeader;
